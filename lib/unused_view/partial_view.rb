d2ae1b63fe925928c2e31d42929cb951fa94b062
Takatoshi Matsumoto <toqoz403@gmail.com>
2014-03-16 23:58:27 +0900

    Find a partial view depend on a partial view

--- lib/unused_view/partial_view.rb
+++ lib/unused_view/partial_view.rb
@@ -2,6 +2,8 @@
 
 module UnusedView
   class PartialView
+    class UnresolvePartial < StandardError; end
+
     PATTERN = {
       render: %r{\brender\s*(?:\(\s*)?},
       partial: %r{:partial\s*=>\s*|partial:\s*},
@@ -14,26 +16,44 @@ module UnusedView
     end
 
     def find_all
+      r = _find_all
+
+      if r.count > 0
+        r + PartialView.new(r).find_all
+      else
+        r
+      end
+    end
+
+    def _find_all
       @views.reduce([]) do |sum, view|
         used_partials_by_view = File.open(view) { |f| f.read.split("\n") }.map do |line|
           if line =~ %r[(?:#{PATTERN[:partial]}|#{PATTERN[:render]})(['"])/?(#{PATTERN[:filename]})#{PATTERN[:extention]}*\1]
             $2 # virtual path. e.g. articles/form
           end
-        end.compact.map do |vpath|
-          File.class_eval do
-            if vpath.index('/')
-              path = ApplicationController.view_paths.map do |view_path|
-                File.join(view_path.to_s, vpath)
-              end.select { |f| File.exists?(File.dirname(f)) }.first || vpath
-            else
-              path = view
-            end
-            filename = "_#{basename(vpath)}.#{basename(view).split('.')[1..-1].join('.')}"
-            expand_path(join(dirname(path), filename)).to_s
-          end
+        end.compact.map do |partial|
+          resolve view, partial
         end
+
         sum + used_partials_by_view
       end.uniq
+    end
+
+    def resolve view, partial
+      ext = File.basename(view).split('.')[1..-1].join('.') # foo.html.erb -> html.erb
+
+      view_paths = [File.dirname(view)] + default_view_paths.map(&:to_s)
+
+      r = view_paths.
+        map { |view_path| File.join(view_path.to_s, File.dirname(partial), "_#{File.basename(partial)}.#{ext}") }.
+        detect { |f| File.exists?(f) }
+
+      raise UnresolvePartial.new("#{partial} in #{view}") unless r
+      File.expand_path(r).to_s
+    end
+
+    def default_view_paths
+      ApplicationController.view_paths
     end
   end
 end
--- spec/lib/unused_view/partial_view_spec.rb
+++ spec/lib/unused_view/partial_view_spec.rb
@@ -11,37 +11,56 @@ describe UnusedView::PartialView do
   end
 
   describe '#find_all' do
-    let(:views) { [ Rails.root.join('app/views/test/new.html.erb').to_s ] }
-    let(:partials) {
+    let!(:views) {
       [
-        Rails.root.join('app/views/test/_piyo.html.erb').to_s,
-        Rails.root.join('app/views/shared/_hogessu.html.erb').to_s
+        Rails.root.join('app/views/test/new.html.erb').to_s.tap { |path|
+          FileUtils.mkdir_p(File.dirname(path)) unless File.directory? File.dirname(path)
+          File.open(path, 'w+') { |f|
+            f.write <<-EOS
+              <% render "piyo" %>'
+              <% render partial: "shared/hogessu" %>
+            EOS
+          }
+        }
       ]
     }
-    let(:unused_partials) {
-      [ Rails.root.join('app/views/test/_unused.html.erb').to_s ]
-    }
-    before(:each) do
-      views.each do |v|
-        FileUtils.mkdir_p(File.dirname(v)) unless File.directory? File.dirname(v)
-        File.open(v, 'w+') { |f|
-          f.write <<-EOS
-            <% render "piyo" %>'
-            <% render "shared/hogessu" %>
-          EOS
+
+    let!(:partials) {
+      [
+        Rails.root.join('app/views/test/_piyo.html.erb').to_s.tap { |path|
+          FileUtils.mkdir_p(File.dirname(path)) unless File.directory? File.dirname(path)
+          File.open(path, 'w+') { |f|
+            f.write <<-EOS
+              <% render "piyopiyo" %>
+            EOS
+          }
+        },
+        Rails.root.join('app/views/test/_piyopiyo.html.erb').to_s.tap { |path|
+          FileUtils.mkdir_p(File.dirname(path)) unless File.directory? File.dirname(path)
+          FileUtils.touch path
+        },
+        Rails.root.join('app/views/shared/_hogessu.html.erb').to_s.tap { |path|
+          FileUtils.mkdir_p(File.dirname(path)) unless File.directory? File.dirname(path)
+          FileUtils.touch path
         }
-      end
-      partials.each do |v|
-        FileUtils.mkdir_p(File.dirname(v)) unless File.directory? File.dirname(v)
-        FileUtils.touch v
-      end
-      unused_partials.each do |v|
-        FileUtils.mkdir_p(File.dirname(v)) unless File.directory? File.dirname(v)
-        FileUtils.touch v
-      end
-    end
+      ]
+    }
+
+    let!(:unused_partials) {
+      [
+        Rails.root.join('app/views/test/_unused.html.erb').to_s.tap { |path|
+          FileUtils.mkdir_p(File.dirname(path)) unless File.directory? File.dirname(path)
+          FileUtils.touch path
+        }
+      ]
+    }
+
     it {
-      described_class.new(views).find_all.should eq(partials)
+      partials.each do |partial|
+        expect(described_class.new(views).find_all).to include(partial)
+      end
+
+      expect(described_class.new(views).find_all).not_to include(unused_partials)
     }
   end
 end
